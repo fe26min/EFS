7a3e01d3f9747467be42af096bf8967c96f6c8ed
Fe26min <118718073+fe26min@users.noreply.github.com>
2023-11-01 19:20:15 +0900

    feat : add weather view
    날씨 아이콘 추가
    최고 온도 최저 온도 현재 온도

---
+++ EscapeFromSofa/app/src/main/java/com/fe26min/efs/WeatherView.kt
@@ -0,0 +1,9 @@
+package com.fe26min.efs
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+
+class WeatherView(context: Context, attrs: AttributeSet) : View(context, attrs) {
+
+}
