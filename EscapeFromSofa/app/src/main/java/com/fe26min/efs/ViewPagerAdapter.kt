44b665231a46eb043ad3452da42bf30ce7a6d804
Fe26min <118718073+fe26min@users.noreply.github.com>
2023-11-01 15:27:06 +0900

    dev : main
    실제 어플용 앱 main 브런치

--- EscapeFromSofa/app/src/main/java/com/fe26min/efs/ViewPagerAdapter.kt
+++ EscapeFromSofa/app/src/main/java/com/fe26min/efs/ViewPagerAdapter.kt
@@ -6,19 +6,23 @@ import androidx.viewpager2.adapter.FragmentStateAdapter
 class ViewPagerAdapter(private val mainActivity: MainActivity) : FragmentStateAdapter(mainActivity) {
 
     override fun getItemCount(): Int {
-        return 3
+        return 1
+    //        return 3
     }
 
     override fun createFragment(position: Int): Fragment {
         return when(position) {
-            0 -> {
-                return C25kFragment()
-            }
-            1 -> {
-                return Zone2Fragment()
-            }
+//            0 -> {
+//                return C25kFragment()
+//            }
+//            1 -> {
+//                return Zone2Fragment()
+//            }
+//            else -> {
+//                return RecordFragment()
+//            }
             else -> {
-                return RecordFragment()
+                return C25kFragment()
             }
         }
     }
